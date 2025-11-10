f79c05323d8e1785b176e2bdb3b66a4c1361976f
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-11-10 16:29:51 +0900

    env 적용

--- app.py
+++ app.py
@@ -2,12 +2,19 @@ from flask import Flask
 from flask_restx import Api
 from database import db
 from resources import init_routes
+from dotenv import load_dotenv
 import logging
 import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@mysql:3306/board'
+load_dotenv()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@"
+    f"{os.getenv('DB_DOCKER_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}")
+
+# mysql+pymysql://root:1234@mysql:3306/board
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
