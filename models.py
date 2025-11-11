34e30cfa56625b520eb5bf90f3a91ab4657008f3
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-11-11 16:55:01 +0900

    comment 추가 및 board 컬럼 추가

--- models.py
+++ models.py
@@ -1,10 +1,24 @@
 from database import db
+from datetime import datetime
 
-class Task(db.Model):
+class Board(db.Model):
     __tablename__ = 'board'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(20), nullable=False)
     content = db.Column(db.String(100), nullable=False)
+    likes = db.Column(db.Integer, default=0, nullable=False)
+    created_at=db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return self.title
+    
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id = db.Column(db.Integer, primary_key=True)
+    content=db.Column(db.String(100), nullable=False)
+    likes=db.Column(db.Integer, default=0, nullable=False)
+    parent_comment_id = db.Column(db.Integer, db.ForeignKey('comment.id'), nullable=True)
+    created_at=db.Column(db.DateTime, default=datetime.utcnow)
+    board_id=db.Column(db.Integer, db.ForeignKey('board.id'), nullable=False)
+    
+    replies = db.relationship('Comment', backref=db.backref('parent', remote_side=[id]), lazy=True)
